5c6aa225ce9f057a8de3cd789512534baa4f8a1a
Maciej <maciej.krzykwa@gmail.com>
2025-05-29 20:37:52 +0200

    Remix config added

---
+++ remix.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  serverBuildTarget: "netlify",
+  future: {
+    // jeśli chcesz włączyć jakieś eksperymentalne flagi
+  }
+};

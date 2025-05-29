4cb19c0dc4018c3ce8b36ad99578578c001025ec
Maciej <maciej.krzykwa@gmail.com>
2025-05-30 00:41:27 +0200

    Adding CORS.

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,16 @@
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+  origin: "https://nutrifytrackerv2.netlify.app/", // adres Twojego frontendu
+  credentials: true,                           // jeśli używasz ciasteczek
+}));
 
 app.use((req, res, next) => {
   const start = Date.now();

121d6408bc00c21d3830372d16a556c5ceceeba7
Maciej <maciej.krzykwa@gmail.com>
2025-05-29 22:22:31 +0200

    Revert "Remix config added"
    This reverts commit 5c6aa225ce9f057a8de3cd789512534baa4f8a1a.

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-module.exports = {
-  serverBuildTarget: "netlify",
-  future: {
-    // jeśli chcesz włączyć jakieś eksperymentalne flagi
-  }
-};
